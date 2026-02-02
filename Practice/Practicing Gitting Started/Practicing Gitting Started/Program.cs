021c59fd768c576b46168e91d40921edeaf54d1b
LOVELACE, CHRISTIAN <cl0786465@otc.edu>
2026-02-02 12:19:12 -0600

    Initial commit.

---
+++ Practice/Practicing Gitting Started/Practicing Gitting Started/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practicing_Gitting_Started
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
